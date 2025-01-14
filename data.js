119f61723ac5d9908f4d1e16f28101dd84060ab8
tom8opot8o <140798931+tom8opot8o@users.noreply.github.com>
2025-01-14 17:21:55 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const pageData = {
+    accountName: "crowded_kitchen",
+    posts: [
+        {
+            image: "image1.jpg",
+            link: "https://customers.dev.instacart.tools/store/recipes/3491819"
+        },
+        {
+            image: "image2.jpg",
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fmiranda%2F&aff_id=5295&offer_id=1&content_id=ccc0b4f5-e983-4d29-819d-87c54c5165e3&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
+        },
+        {
+            image: "image3.jpg",
+            link: "https://www.instacart.com/store/partner_recipes?recipeSourceUrl=https%3A%2F%2Ftom8opot8o.github.io%2Fmiranda%2F&aff_id=5295&offer_id=1&content_id=ccc0b4f5-e983-4d29-819d-87c54c5165e3&utm_source=instacart_growth_partnerships&utm_medium=affiliate_recipe_unknown"
+        }
+    ]
+};
